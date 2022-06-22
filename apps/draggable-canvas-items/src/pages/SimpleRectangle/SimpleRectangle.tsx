6d9c1ff175a571737f1cf30a3830a9cb9437bb22
pitiboy <peter.horvath@finnoconsult.at>
2022-06-22 13:31:20 +0200

    add input to manage position

--- apps/draggable-canvas-items/src/pages/SimpleRectangle/SimpleRectangle.tsx
+++ apps/draggable-canvas-items/src/pages/SimpleRectangle/SimpleRectangle.tsx
@@ -18,40 +18,53 @@ const Container = styled.div`
   outline: solid 1px black;
   width: 500px;
   height: 200px;
+  padding: 0;
 `;
 const DragItem = styled.div`
   border: solid 10px rgba(255, 0,0,0.2);
-  margin: 10px;
-  width: 100px;
-  height: 100px;
+  width: 66px;
+  height: 66px;
 `;
 const Hole = styled.div`
   border: dashed 1px black;
   height: 100%;
 `;
 
+
 export function SimpleRectangle(props: SimpleRectangleProps) {
 
   const container = React.useRef<HTMLDivElement | null>(null);
+
+  const [x, setX] = React.useState(0);
+  const [y, setY] = React.useState(0);
   const item = React.useRef<HTMLDivElement | null>(null);
   const item2 = React.useRef<HTMLDivElement | null>(null);
 
   React.useLayoutEffect(() => {
+    (window as any).gsap = gsap;
+  }, []);
+
+  React.useLayoutEffect(() => {
     if (container && item && container.current && item.current) {
-      Draggable.create(item.current, {
+      (window as any).draggables = Draggable.create(item.current, {
         bounds: container.current,
+        onDragEnd: ({x, y}) => {setX(x); setY(y)},
       });
     }
   }, [container, item]);
 
   React.useLayoutEffect(() => {
     if (container && item2 && container.current && item2.current) {
-      Draggable.create(item2.current, {
+      (window as any).draggables = Draggable.create(item2.current, {
         bounds: container.current,
+        // snap: gsap.utils.snap(1),
       });
     }
   }, [container, item]);
 
+
+  const moveLeft = (x: number) => { setX(x); gsap.to(item.current, {x});}
+  const moveTop = (y: number) => { setY(y); gsap.to(item.current, {y});}
 
   return (
     <StyledSimpleRectangle>
@@ -59,7 +72,10 @@ export function SimpleRectangle(props: SimpleRectangleProps) {
         <DragItem ref={item}  style={{borderRadius: '100%'}}><Hole   style={{borderRadius: '100%'}}/></DragItem>
         <DragItem ref={item2}><Hole /></DragItem>
       </Container>
-      <h1>Welcome to SimpleRectangle!</h1>
+
+      left <input type="number" value={x} onChange={(e) => moveLeft(+e.target.value)} min="0" max="500" />
+      <br/>
+      top: <input type="number" value={y} onChange={(e) => moveTop(+e.target.value)} min="0" max="200" />
     </StyledSimpleRectangle>
   );
 }
